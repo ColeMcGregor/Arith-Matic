df0d9176b32e91ebb3a3a43df9ee91b52681a7f7
ColeMcGregor <gc28mcgr@siena.edu>
2025-09-08 22:52:24 -0400

    Feat(src:vm): added in stub for GameViewModel file. pretty much empty

---
+++ app/src/main/java/com/colemcg/arith_matic/vm/GameViewModel.kt
@@ -0,0 +1,10 @@
+/**
+ * This is the view model for the game
+ * it is used as the temporary storage for the UI of the game, and is fed by the model
+ * 
+ * @author 
+ */
+
+ //TODO: add the view model
+
+
