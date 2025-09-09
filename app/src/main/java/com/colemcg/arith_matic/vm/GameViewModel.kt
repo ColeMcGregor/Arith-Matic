93cbe48cd951bf98603d881d03a05c2ffe15b83a
ColeMcGregor <gc28mcgr@siena.edu>
2025-09-09 18:38:41 -0400

    Feat(src:vm): added in GameViewModel in a stubbed format to be used by the fragments ot update the information and game

--- app/src/main/java/com/colemcg/arith_matic/vm/GameViewModel.kt
+++ app/src/main/java/com/colemcg/arith_matic/vm/GameViewModel.kt
@@ -1,3 +1,5 @@
+package com.colemcg.arith_matic.vm
+
 /**
  * This is the view model for the game
  * it is used as the temporary storage for the UI of the game, and is fed by the model
@@ -5,6 +7,44 @@
  * @author 
  */
 
- //TODO: add the view model
 
+import com.colemcg.arith_matic.model.GameSettings
+import com.colemcg.arith_matic.model.QuestionCard
+import com.colemcg.arith_matic.model.GameResults
+import com.colemcg.arith_matic.generation.CardFactory
+import com.colemcg.arith_matic.storage.SettingsDataStore
 
+@Suppress("MemberVisibilityCanBePrivate", "UNUSED_PARAMETER")
+class GameViewModel(
+    private val cardFactory: CardFactory,
+    private val settingsDataStore: SettingsDataStore
+) {
+    // Fields from the reference diagram
+    private var gameSettings: GameSettings? = null
+    private val questionList: MutableList<QuestionCard> = mutableListOf()
+    private var currentIndex: Int = 0
+    private var score: Int = 0
+    private var streak: Int = 0
+
+    fun setSettings(settings: GameSettings) {
+        // intentionally empty (stub)
+    }
+
+    fun startGame() {
+        // intentionally empty (stub)
+    }
+
+    fun getNextQuestion(): QuestionCard {
+        // intentionally empty (stub)
+        throw NotImplementedError("stub")
+    }
+
+    fun recordAnswer(correct: Boolean, card: QuestionCard) {
+        // intentionally empty (stub)
+    }
+
+    fun getResults(): GameResults {
+        // intentionally empty (stub)
+        throw NotImplementedError("stub")
+    }
+}
