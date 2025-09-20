697515f5115b0ccd478ae2a360c376d90a62820a
JardinaG <ja19gome@siena.edu>
2025-09-20 01:49:22 -0400

    feat(theme): added classroom-imspred color palette for app UI based on mockup made previously

---
+++ Android/app/src/main/java/com/colemcg/arith_matic/ui/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.colemcg.arith_matic.ui.theme
+
+import androidx.compose.ui.graphics.Color
+//TODO: add more colors to fit mockup
+/**
+ *  centralized collection of colors used throughout the UI.
+ * A color palette inspired by a traditional classroom setting mockup previously made.
+ *
+ * @author Jardina Gomez
+ */
+
+    val ChalkboardGreen = Color(0xFF196739)
+    val ClassroomWallYellow = Color(0xFFFdda62)
+    val QuestionText = Color(0xFFFFC107)
+
+    // Answer box colors
+    val AnswerGreen = Color(0xFF88c05b)
+    val AnswerYellow = Color(0xFFf9c538)
+    val AnswerOrange = Color(0xFFff9454)
+    val AnswerTeal = Color(0xFF76c7a7)
+
+    // Furniture and props
+    val AppleRed = Color(0xFFE12F2F)
+    val DeskWoodOrange = Color(0xFF915729)
+    val ChairWoodLight = Color(0xFFD78734)
+
+    // Optional text or background colors
+    val PureWhite = Color(0xFFFFFFFF)
+    val PureBlack = Color(0xFF000000)
+
