a6e91a09afa7dee7c49f51335fbea64731f14c59
JardinaG <ja19gome@siena.edu>
2025-09-23 18:22:10 -0400

    fix: Fixed the implementation of the google fonts dependency since we are using Compose BOM

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -61,6 +61,6 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)  // if/when you add a ViewModel
     implementation(libs.androidx.navigation.compose)        // if using Compose navigation
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.9.1") // Google Fonts in Compose
+    implementation("androidx.compose.ui:ui-text-google-fonts") // Google Fonts in Compose
 
 }
